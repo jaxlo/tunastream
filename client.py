2aab1a847b12839280f294b7960fc2019fb808e0
Jackson Lohman <jacksonlohman@gmail.com>
2020-02-18 09:30:54 -0700

    Get the timer to work?

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from tunastream import Tuna
+from timeit import default_timer as timer
 
-while True:
-    Tuna.send(18650, 'ltp', 'largeImg.png')
-    print('Sent')
+start = timer()
+Tuna.sendFile(18650, '120.0.0.1', '/home/jax/Downloads/manjaro-xfce-18.1.4-191210-linux54.iso')
+print(timer() - start)
